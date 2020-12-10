8b705ef53e8d7fece422802df21198aa63435e1a
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-10 08:52:45 +0300

    Initial Commit

---
+++ HWS10.Project2.CountryFlags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWS10.Project2.CountryFlags
+//
+//  Created by Александр Болотов on 10.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
